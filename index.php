09b9150464c9f750e96d78e99a13d19db4f99fd5
keiji-numata <147024363+keiji-numata@users.noreply.github.com>
2024-03-28 02:33:57 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html>
+<head>
+<title>hello</title>
+</head>
+
+<body>
+    Here is some straight HTML.<br />
+<?php
+	echo "This is created using PHP echo. <br />";
+?>
+<br/><br/>
+<em>Can you tell the difference?</em><br/>
+	
+</body>
+</html>
